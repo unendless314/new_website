63f213b84438045aa88d399ae281305524a70ef9
unendless314 <65386006+unendless314@users.noreply.github.com>
2026-01-01 22:00:14 +0800

    Fix Youtube shortcode import for react-lite-youtube-embed

--- src/components/common/shortcodes/Youtube.tsx
+++ src/components/common/shortcodes/Youtube.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import { LiteYouTubeEmbed } from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 const Youtube = ({
